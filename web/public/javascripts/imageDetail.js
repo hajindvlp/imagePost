4e2ac5a6a0d3479d254189cc92227bf751295358
hajindvlp <harryhajinchung@gmail.com>
2019-03-20 18:58:15 +0900

    .

--- web/public/javascripts/imageDetail.js
+++ web/public/javascripts/imageDetail.js
@@ -1,35 +1,6 @@
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
-
-function getPath(id){
-    var url = 'mongodb://localhost:27017/';
-    var dbName = 'image';
-    var client = new MongoClient(url);
-    var path;
-
-    MongoClient.connect(url, function (err, client) {
-        if (err) {
-        console.log(id);
-            path = "error";
-            return;
-        }
-
-        var db = client.db;
-        var image = db.collection('image');
-        // ObjectId("5c8f38a5a753fbf3f036e7e4")
-        image.findOne({_id : `ObjectId({"${id}"})`}, (err, docs) => {
-            console.log(docs);
-        });
-
-        console.log(db);
-    });
-}
-
 module.exports = {
     HTML:function(id){
-        getPath(id);
         var fileName = `../images/${id}.png`;
-        // console.log(fileName);
         return `
         <!doctype html>
         <html>
--- web/public/javascripts/imageList.js
+++ web/public/javascripts/imageList.js
@@ -1,24 +1,27 @@
+var fs = require('fs');
+
+function getList(){
+    var list;
+    fs.readdirSync("../images").forEach(file => {
+        list+=`<a href="../images/${file}" style="margin:5px"><img src="../images/${file}" height="42" width="42"></a>`;
+    });
+    return list;
+}
+
 module.exports = {
-  HTML:function(id){
-    return `
-    <!doctype html>
-    <html>
-        <head>
-            <meta charset="utf-8">
-            <link rel="stylesheet" type="text/css" href="/stylesheets/style.css">
-        </head>
-        <body>
-              <div id = "image">
-                  Rendering...
-              </div>
-
-              <script type="text/javascript">
-                  var fileName = 'images/'+${id}.toString()+'.png';
-
-                  document.getElementById('image').innerHTML = <img src=${fileName}>;
-              </script>
-        </body>
-    </html>
-    `;
-  }
+    HTML:function(){
+        var list = getList();
+        return `
+        <!doctype html>
+        <html>
+            <head>
+                <meta charset="utf-8">
+                <link rel="stylesheet" type="text/css" href="/stylesheets/style.css">
+            </head>
+            <body>
+                ${list}
+            </body>
+        </html>
+        `;
+    }
 }

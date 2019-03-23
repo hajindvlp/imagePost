e2fe788d07d59e48018c0a974e1e64f67e578ca9
hajindvlp <harryhajinchung@gmail.com>
2019-03-23 20:34:29 +0900

    v1.0

--- web/lib/javascripts/imageDetail.js
+++ web/lib/javascripts/imageDetail.js
@@ -1,33 +1,21 @@
+var template = require('./template.js');
+
 module.exports = {
     HTML:function(id){
-        var fileName = `../images/${id}`;
+        var head = template.head().toString();
+        var header = template.header().toString();
         return `
         <!doctype html>
         <html>
             <head>
-                <meta charset="utf-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <link rel="stylesheet" href="/stylesheets/style.css">
+                ${head}
             </head>
             <body>
                 <div id="header">
-                    <h1>소혜 데이터베이스</h1>
-                    <img src="/cover.png" alt="cover" class="headImage">
-                    <ul class="navigator">
-                        <li class="category"><a href="/" class="link">홈</a></li>
-                        <li class="category"><a href="/image" class="link">갤러리</a></li>
-                        <li class="category"><a href="/image/upload" class="link">업로드</a></li>
-                    </ul>
-                    <br/>
+                    ${header}
                 </div>
                 <div id="content">
-                    <div id="imageGrid">
-                        Rendering...
-                    </div>
-
-                    <script type="text/javascript">
-                        document.getElementById('imageGrid').innerHTML = '<img src="${fileName}" id="imageDetail">';
-                    </script>
+                    <img src="/images/${id}" id="imageDetail">
                 </div>
             </body>
         </html>
--- web/lib/javascripts/index.js
+++ web/lib/javascripts/index.js
@@ -19,7 +19,8 @@ module.exports = {
                 <div id="content">
                     2019.03.20~2019.03.23 동안 한 펭귄이 개발함...<br>
                     사진 많이 많이 올려주세용~~
-                    <div id="red">부적합한 사진 올릴시 이용 제한당할 수 있습니다.</div>
+                    <div id="red">부적합한 사진 올릴시 이용 제한당할 수 있습니다.</div><br>
+                    <a href="https://github.com/hajindvlp/imageSwipe">궁금하신 분들 참고하세요</a>
                 </div>
             </body>
         </html>
